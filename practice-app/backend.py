f44efbe056389b2e57ac9ce586170ed1e34bc962
hazerb <60961048+hazerb@users.noreply.github.com>
2020-05-25 19:04:16 +0300

    Update backend.py

--- practice-app/backend.py
+++ practice-app/backend.py
@@ -134,55 +134,6 @@ def api_coronavirusByCountry():
 def dashboard():
     return render_template('dashboard.html')
 
-#Template for POST request
-@app.route('/endUser', methods=['POST'])
-def endUser():
-
-    if request.method == 'POST':
-        if request.form["type"] == "AUTHOR":
-
-            string = ""
-            string = "<h1>NAMES </h1><br>" + string
-            return string + "<br> " + '<br><br> <a href=" / "> Return to home page </a>'
-
-        elif request.form["type"] == "PAPER":
-
-            string = "<h1> AUTHORS ------ TITLE ------ ABSTRACT ------ TOPICS ------ RESULT</h1><br>"
-            return (string + "<br> " + '<br> <a href=" / "> Return to home page </a>')
-
-
-        elif request.form["type"] == "TOPIC":
-            string = "<h1> TOPIC NAME ------- SOTA </h1>"
-            return string + '<br> <a href=" / "> Return to home page </a>'
-
-        elif request.form["type"] == "getAuthorPapers":
-            string = 'ALL PAPERS OF ' + request.form["authorName"] + "<br>"
-            string += "<h1> AUTHORS ------ TITLE ------ ABSTRACT ------ TOPICS ------ RESULT</h1><br>"
-            return (string + "<br> " + '<br> <a href=" / "> Return to home page </a>')
-
-        elif request.form["type"] == "sotaResultByTopic":
-            string = "SOTA RESULT BY TOPIC <br>" + "<h1>TOPIC ----- SOTA  ------------------- IN WHICH PAPER </h1><br>"
-            return string + '<br> <a href=" / "> Return to home page </a>'
-
-
-
-        elif request.form["type"] == "getPapersByTopic":
-            string += "<h1> AUTHORS ------ TITLE ------ ABSTRACT ------ TOPICS ------ RESULT</h1><br>"
-            return (string + "<br> " + '<br> <a href=" / "> Return to home page </a>')
-
-        elif request.form["type"] == "searchKeyword":
-            #Database Fetch
-            string += "<h1> AUTHORS ------ TITLE ------ ABSTRACT ------ TOPICS ------ RESULT</h1><br>"
-            return (string + "<br> " + '<br> <a href=" / "> Return to home page </a>')
-
-        elif request.form["type"] == "searchCo-authors":
-            string = '<h1>CoAuthors Of Author "' + request.form[
-                "authorName"] + '"</h1><br>'
-            return string + '<br> <a href=" / "> Return to home page </a>'
-
-    return "error"
-
-
 if __name__ == '__main__':
     #api.add_resource(scholar_util.SearchAuthorName,'/authornamesearch')
     #api.add_resource(scholar_util.AuthorPublic,'/authorpublications')
