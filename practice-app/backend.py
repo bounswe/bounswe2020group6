731109ce8b8879e7663ef48e5e5376c804ec1a75
hazerb <60961048+hazerb@users.noreply.github.com>
2020-05-25 18:36:22 +0300

    Update backend.py

--- practice-app/backend.py
+++ practice-app/backend.py
@@ -67,33 +67,6 @@ def api_authorstats():
     countryData = scholar_util.getAuthorCitationStats(name)
     return jsonify(json)  
 
-
-@app.route('/profile',methods=['POST'])
-def profile():
-    if request.method=='POST':
-        authorJson=scholar_util.search_authors_by_name(request.form["name"])
-        print(authorSearchResult)
-
-        #TODO:Get these info and add them to profile.html page
-        scholar_util.getNameOutOfAuthorJson(authorJson)
-        scholar_util.getAuthorsColloborators(authorJson)
-        scholar_util.getAuthorsCitationIndexes(authorJson)
-        scholar_util.getAuthorPhoto(authorJson)
-        scholar_util.getAuthorsRecentPublications(authorJson)
-
-        return render_template('profile.html')
-    
-    
-    else:
-        return "Error 404"
-        print("get")
-
-
-        context = {}
-
-    return render_template('search.html', context=context)
-
-
 @app.route('/coronavirus', methods=['GET'])
 def coronavirus():
    
@@ -107,7 +80,6 @@ def api_coronavirus():
     countryData = coronavirus_api.coronavirus_summary_search()
     return jsonify(countryData)
 
-#-------
 @app.route('/coronavirusCountryLive', methods=['POST', 'GET'])
 def coronavirusCountryLive():
    
@@ -140,7 +112,7 @@ def api_coronavirusCountryLive():
         context = {}
 
     return context
-#-------
+
 
 @app.route('/api/worldStats', methods=['GET'] )
 def api_world_stats():
@@ -186,61 +158,7 @@ def api_coronavirusByCountry():
 def dashboard():
     return render_template('dashboard.html')
 
-#Template for POST request
-@app.route('/endUser', methods=['POST'])
-def endUser():
-
-    if request.method == 'POST':
-        if request.form["type"] == "AUTHOR":
-
-            string = ""
-            string = "<h1>NAMES </h1><br>" + string
-            return string + "<br> " + '<br><br> <a href=" / "> Return to home page </a>'
-
-        elif request.form["type"] == "PAPER":
-
-            string = "<h1> AUTHORS ------ TITLE ------ ABSTRACT ------ TOPICS ------ RESULT</h1><br>"
-            return (string + "<br> " + '<br> <a href=" / "> Return to home page </a>')
-
-
-        elif request.form["type"] == "TOPIC":
-            string = "<h1> TOPIC NAME ------- SOTA </h1>"
-            return string + '<br> <a href=" / "> Return to home page </a>'
-
-        elif request.form["type"] == "getAuthorPapers":
-            string = 'ALL PAPERS OF ' + request.form["authorName"] + "<br>"
-            string += "<h1> AUTHORS ------ TITLE ------ ABSTRACT ------ TOPICS ------ RESULT</h1><br>"
-            return (string + "<br> " + '<br> <a href=" / "> Return to home page </a>')
-
-        elif request.form["type"] == "sotaResultByTopic":
-            string = "SOTA RESULT BY TOPIC <br>" + "<h1>TOPIC ----- SOTA  ------------------- IN WHICH PAPER </h1><br>"
-            return string + '<br> <a href=" / "> Return to home page </a>'
-
-
-
-        elif request.form["type"] == "getPapersByTopic":
-            string += "<h1> AUTHORS ------ TITLE ------ ABSTRACT ------ TOPICS ------ RESULT</h1><br>"
-            return (string + "<br> " + '<br> <a href=" / "> Return to home page </a>')
-
-        elif request.form["type"] == "searchKeyword":
-            #Database Fetch
-            string += "<h1> AUTHORS ------ TITLE ------ ABSTRACT ------ TOPICS ------ RESULT</h1><br>"
-            return (string + "<br> " + '<br> <a href=" / "> Return to home page </a>')
-
-        elif request.form["type"] == "searchCo-authors":
-            string = '<h1>CoAuthors Of Author "' + request.form[
-                "authorName"] + '"</h1><br>'
-            return string + '<br> <a href=" / "> Return to home page </a>'
-
-    return "error"
-
 
 if __name__ == '__main__':
-    #api.add_resource(scholar_util.SearchAuthorName,'/authornamesearch')
-    #api.add_resource(scholar_util.AuthorPublic,'/authorpublications')
-    #api.add_resource(scholar_util.SearchPublication,'/publicationsearch')
-    #api.add_resource(scholar_util.AuthorCitationStats,'/authorstats')
-    #api.add_resource(coronavirus_api.countryLive, '/countryLive')
-    #api.add_resource(coronavirus_api.CoronavirusByCountry, '/coronavirusbycountry')
     app.run(debug=True)
 
