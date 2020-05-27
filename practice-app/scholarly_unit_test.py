21c756c0081fccc1a8f9ca2e03a92920bf28b361
isiktashamza <44121357+isiktashamza@users.noreply.github.com>
2020-05-27 18:36:40 +0300

    add unittest for search endpoint.

--- practice-app/scholarly_unit_test.py
+++ practice-app/scholarly_unit_test.py
@@ -2,6 +2,7 @@ import unittest
 import json
 import scholar_util
 
+
 def isGetAuthorsPublicationsCorrect():
     response = scholar_util.getAuthorsPublications("Ali")
     print("Hello")
@@ -10,22 +11,35 @@ def isGetAuthorsPublicationsCorrect():
             isCorrect = True
             for publication in response["publications"]:
                 isCorrect = (isCorrect
-                            and type(publication["title"]) == str
-                            and type(publication["author"]) == str
-                            and type(publication["summary"]) == str
-                            and type(publication["year"]) == str
-                            and type(publication["url"]) == str)
+                             and type(publication["title"]) == str
+                             and type(publication["author"]) == str
+                             and type(publication["summary"]) == str
+                             and type(publication["year"]) == str
+                             and type(publication["url"]) == str)
 
     return isCorrect
 
+
 def isSearchCorrect():
-    
-    json = scholar_util.searchPublication("cell")     
-    
+    json = scholar_util.searchPublication("cell")
+
     if 'FM Gill' not in json["author"]:
         return False
     else:
         return True
+
+
+def isGetAuthorCorrect():
+    for i in range(1, 10):
+        json = scholar_util.getAuthors("Hawking", i)["author_search_result"]
+        for author in json:
+            if (author["name"] is None or
+                    author["affiliation"] is None or
+                    author["url_picture"] is None or
+                    author["id"] is None):
+                return False
+        return True
+
 
 class ScholarlyTest(unittest.TestCase):
 
@@ -33,6 +47,8 @@ class ScholarlyTest(unittest.TestCase):
     def test(self):
         self.assertTrue(isGetAuthorsPublicationsCorrect() == True)
         self.assertTrue(isSearchCorrect() == True)
+        self.assertTrue(isGetAuthorCorrect() == True)
+
 
 if __name__ == '__main__':
     unittest.main()
