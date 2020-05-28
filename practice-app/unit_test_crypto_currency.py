985fdfbf34e9819a8427a6a1e039f20273d9685e
Emilcan Arican <emilcan.arican@boun.edu.tr>
2020-05-28 18:23:14 +0300

    unit test name correction

--- practice-app/unit_test_crypto_currency.py
+++ practice-app/unit_test_crypto_currency.py
@@ -19,11 +19,59 @@ def isGetCoinRanksCorrect():
     return isCorrect
 
 
+def isGetBitcoinPriceCorrect():
+    response = bitcoin_api.getBitcoinPrice()
+
+    currencies = ["EUR", "USD", "GBP"]
+    attributes = ["code", "description", "rate", "rate_float", "symbol"]
+
+    looks_right = (
+
+        # CHECK IF THERE WAS A RESPONSE
+        response
+
+        # CHECK IF DISCLAIMER IS IN PLACE
+        # AND OF THE CORRECT TYPE
+        and response["disclaimer"]
+        and type(response["disclaimer"]) == str
+
+        # CHECK IF TIME UPDATED IS IN PLACE
+        # AND OF THE CORRECT TYPE
+        and response["time_updated"]
+        and type(response["time_updated"]) == str
+
+        # CHECK IF THE RATES ARE IN PLACE
+        and response["rates"]
+    )
+
+    # IF SO FAR SO GOOD,
+    # CHECK IF THE RETURNED JSON HAS ALL THREE CURRENCIES
+    # AND ALL ATTRIBUTES THAT SHOULD EXIST FOR A CURRENCY
+    if looks_right:
+        for currency_key in currencies:
+                if currency_key not in response["rates"]:
+                    return False
+                else:
+                    for attribute in attributes:
+                        if attribute not in response["rates"][currency_key]:
+                            return False
+        return True
+    else:
+        return False
+
+
+
 class AltCoinTest(unittest.TestCase):
 
     # Returns True or False.
     def test(self):
         self.assertTrue(isGetCoinRanksCorrect() == True)
+
+class BitcoinTest(unittest.TestCase):
+
+    # Returns True or False.
+    def test(self):
+        self.assertTrue(isGetBitcoinPriceCorrect() == True)
 
 if __name__ == '__main__':
     unittest.main()
