608230fdfe588eaf39522c75126a92ee5fbb398d
Ali Ramazan Mert <alimert@Ali-MacBook-Pro.local>
2020-11-09 13:43:28 +0300

    landing page and login form initial setup

--- frontend/src/pages/Landing/index.jsx
+++ frontend/src/pages/Landing/index.jsx
@@ -1,39 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Layout, Row, Col } from "antd";
-import { Header, Logo, footerStyle } from "./style";
+import { Layout, Row, Col, Form, Input, Checkbox } from "antd";
+import {
+  Header,
+  Logo,
+  footerStyle,
+  CtaButton,
+  Nav,
+  LoginModal,
+  LoginTitle,
+  LoginButton,
+  LoginLabel,
+} from "./style";
 
 const { Footer } = Layout;
 
-const navStyle = {
-  textAlign: "center",
-  marginTop: "6px",
-  color: "white",
-  fontSize: "18px",
-  fontWeight: "500",
-  fontFamily: "Red Hat Display",
-};
-
 const Landing = () => {
+  const [loginVisible, setLoginVisible] = useState(false);
+
   return (
     <Layout>
+      <LoginModal
+        width={"350px"}
+        visible={loginVisible}
+        footer={null}
+        onCancel={() => setLoginVisible(false)}
+      >
+        <LoginTitle>Login</LoginTitle>
+        <Form layout="vertical">
+          <Form.Item
+            label={<LoginLabel>Email</LoginLabel>}
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label={<LoginLabel>Password</LoginLabel>}
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 12 }} name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <LoginButton type="primary" htmlType="submit">
+              Login
+            </LoginButton>
+          </Form.Item>
+        </Form>
+      </LoginModal>
       <Header style={{ width: "100%" }}>
         <Row justify="center">
-          <Col style={navStyle} span={2}>
-            Menu
-          </Col>
-          <Col style={navStyle} span={2}>
-            Events
-          </Col>
+          <Nav span={2}>Menu</Nav>
+          <Nav span={2}>Events</Nav>
           <Col span={3}>
-            <Logo />
+            <Logo>akademise</Logo>
           </Col>
-          <Col style={navStyle} span={2}>
-            About Us
-          </Col>
-          <Col style={navStyle} span={2}>
+          <Nav span={2}>About Us</Nav>
+          <Nav onClick={() => setLoginVisible(true)} span={2}>
             Login
-          </Col>
+          </Nav>
         </Row>
       </Header>
       <div style={{ backgroundImage: "url(cactus.jpeg)", backgroundSize: "cover", height: "80vh" }}>
@@ -51,8 +83,11 @@ const Landing = () => {
               }}
             >
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-              has been the industry's standard dummy text ever since the 1500s. hello
+              has been the industry's standard dummy text ever since the 1500s.
             </p>
+            <Row justify="center">
+              <CtaButton>JOIN</CtaButton>
+            </Row>
           </Col>
         </Row>
       </div>
--- frontend/src/pages/Landing/style.js
+++ frontend/src/pages/Landing/style.js
@@ -1,11 +1,69 @@
 import styled from "styled-components";
 import theme from "../../theme";
-import { Layout } from "antd";
+import { Layout, Col, Modal } from "antd";
 
 export const footerStyle = {
   backgroundColor: theme.main.colors.third,
   height: "200px",
 };
+
+export const LoginModal = styled(Modal)`
+  left: 160px;
+
+  .ant-modal-content {
+    border-radius: 15px;
+    padding: 0 18px;
+  }
+
+  .ant-checkbox-wrapper:hover .ant-checkbox-inner,
+  .ant-checkbox:hover .ant-checkbox-inner,
+  .ant-checkbox-input:focus + .ant-checkbox-inner {
+    border-color: sandybrown;
+  }
+
+  .ant-checkbox-checked::after {
+    border-color: sandybrown;
+  }
+
+  .ant-checkbox-checked .ant-checkbox-inner {
+    background-color: sandybrown;
+    border-color: sandybrown;
+  }
+
+  .ant-input,
+  .ant-input-affix-wrapper {
+    border-radius: 20px;
+
+    :hover {
+      border-color: ${theme.main.colors.first};
+    }
+  }
+  .ant-input-affix-wrapper .ant-input {
+    border-radius: 0;
+  }
+`;
+
+export const LoginTitle = styled.h1`
+  border-bottom: 3px solid sandybrown;
+`;
+
+export const LoginLabel = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const Nav = styled(Col)`
+  text-align: center;
+  margin-top: 6px;
+  color: white;
+  font-size: 18px;
+  font-weight: 500;
+  cursor: pointer;
+
+  :hover {
+    color: sandybrown;
+  }
+`;
 
 export const Header = styled(Layout.Header)`
   width: 100%;
@@ -18,4 +76,45 @@ export const Logo = styled.div`
   height: 80px;
   width: 100%;
   background-color: ${theme.main.colors.second};
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+export const CtaButton = styled.button`
+  background-color: ${theme.main.colors.first};
+  height: 80px;
+  width: 180px;
+  text-align: center;
+  line-height: 80px;
+  font-size: 32px;
+  font-family: Rasa;
+  color: white;
+  border-radius: 10px;
+  cursor: pointer;
+  border: none;
+
+  :hover {
+    color: sandybrown;
+  }
+`;
+
+export const LoginButton = styled.button`
+  background-color: ${theme.main.colors.first};
+  height: 40px;
+  width: 100%;
+  text-align: center;
+  line-height: 40px;
+  font-size: 16px;
+  font-weight: 500;
+  color: white;
+  border-radius: 10px;
+  cursor: pointer;
+  border: none;
+
+  :hover {
+    color: sandybrown;
+  }
 `;
