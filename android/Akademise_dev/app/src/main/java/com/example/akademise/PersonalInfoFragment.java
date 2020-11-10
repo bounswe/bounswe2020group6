a7752e9bcb541b6ec763ad37b5d65c627775704f
gulperii <34350876+gulperii@users.noreply.github.com>
2020-11-10 22:56:32 +0300

    Personal Info page is created

--- android/Akademise_dev/app/src/main/java/com/example/akademise/PersonalInfoFragment.java
+++ android/Akademise_dev/app/src/main/java/com/example/akademise/PersonalInfoFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.akademise.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,8 +15,26 @@ import androidx.fragment.app.Fragment;
 
 public class PersonalInfoFragment extends Fragment {
     @Nullable
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_personalinfo, container, false);
+        View view = inflater.inflate(R.layout.fragment_personalinfo, container, false);
+
+        return view;
     }
+    @Override
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Spinner uni_select_spinner = (Spinner) getView().findViewById(R.id.uni_select);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                R.array.college_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        uni_select_spinner.setAdapter(adapter);
+
+        Spinner department_select_spinner = (Spinner) getView().findViewById(R.id.department_select);
+        ArrayAdapter<CharSequence> adapter_2 = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                R.array.department_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        department_select_spinner.setAdapter(adapter_2);
+    }
+
 }
