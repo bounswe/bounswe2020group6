ee4539270bb81e0fd5da4cd10c5ad4b03b11eb44
hazerb <60961048+hazerb@users.noreply.github.com>
2020-11-15 18:50:22 +0300

    Add files via upload
    Created function for decoding token

---
+++ backend/util/tokenDecoder.js
@@ -0,0 +1,10 @@
+const atob = require('atob');
+
+function tokenDecoder(req) {
+	const authHeader = req.headers['authorization']
+	const token = authHeader.split(' ')[1]
+	const decodedToken = atob(token.split('.')[1])
+	return JSON.parse(decodedToken)
+}
+
+module.exports = {tokenDecoder}
