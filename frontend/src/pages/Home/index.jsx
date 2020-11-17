d010c3a985634759d54a3a7f24920c12cc3a1b5d
Ali Ramazan Mert <alimert@Ali-MacBook-Pro.local>
2020-11-17 18:04:10 +0300

    redux implementation

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { authAction } from "../../redux/auth/actions";
+
+import axios from "axios";
 
 const Home = () => {
-  return <div>HOME</div>;
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   axios.get("https://api.coindesk.com/v1/bpi/currentprice.json").then((res) => {
+  //     dispatch(authAction(res.data.bpi.USD.rate));
+  //   });
+  // }, []);
+
+  const selector = useSelector;
+  const authToken = selector((state) => state.auth.token);
+
+  return (
+    <div style={{ fontSize: "64px" }}>
+      {authToken}
+      <div>
+        <button
+          onClick={() =>
+            axios.get("https://api.coindesk.com/v1/bpi/currentprice.json").then((res) => {
+              dispatch(authAction(res.data.bpi.USD.rate));
+            })
+          }
+        >
+          login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
