a263c01c270b3dc00fc1d0f74f28b8dbf8d811fb
hazerb <60961048+hazerb@users.noreply.github.com>
2020-11-21 22:23:19 +0300

    Update db.js

--- backend/model/db.js
+++ backend/model/db.js
@@ -56,10 +56,18 @@ const Tag = TagModel(sequelize, Sequelize)
 
 
 
-sequelize.sync()
-  .then(() => {
-    console.log(`Database & tables created!`)
-  })
+sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
+.then(function(){
+    return sequelize.sync({ force: false });
+})
+.then(function(){
+    return sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
+})
+.then(function(){
+    console.log('Database & tables are created.');
+}, function(err){
+    console.log(err);
+});
 
 module.exports = {
   User,
