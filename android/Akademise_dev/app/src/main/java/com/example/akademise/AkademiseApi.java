274a1abef04b2c38d3aca365c61a3b10ed0ccfcf
Tuba Cibuk <tubacibuk1@gmail.com>
2020-11-21 12:55:40 -0800

    AkademiseApi class added. example get post added.

---
+++ android/Akademise_dev/app/src/main/java/com/example/akademise/AkademiseApi.java
@@ -0,0 +1,24 @@
+package com.example.akademise;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface AkademiseApi {
+
+  /*  @Headers({
+            "token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjA1NjIyMzkwfQ.5t4GG2JF-SJB-orXi25rQiaAdw-a27I9Fw_0-IVu8pk"
+    })
+
+   */
+
+    @GET("posts") //relative path (auth/post/add)
+    Call<List<Post>> getPosts();
+
+    @POST("posts")
+    Call<Post> createPost(@Body Post post);
+}
--- android/Akademise_dev/app/src/main/java/com/example/akademise/ProjectCreationActivity.java
+++ android/Akademise_dev/app/src/main/java/com/example/akademise/ProjectCreationActivity.java
@@ -3,11 +3,21 @@ package com.example.akademise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ProjectCreationActivity extends AppCompatActivity {
     Button next;
+    AkademiseApi akademiseApi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +28,17 @@ public class ProjectCreationActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.flPublicationCreation,
                 new ProjectInfoEntryFragment()).commit();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        akademiseApi = retrofit.create(AkademiseApi.class);
+
+        //getPosts();
+        //createPost();
+
     }
 
     View.OnClickListener btnNextClickListener = new View.OnClickListener() {
@@ -32,10 +53,65 @@ public class ProjectCreationActivity extends AppCompatActivity {
                 next.setText(getString(R.string.done));
             }
             else if(next.getText().equals(getString(R.string.done))){
+
+                // TODO: get the content
                 finish();
                 // TODO: add new element to the recycler view.
             }
 
         }
     };
+
+    private  void getPosts(){
+
+        Call<List<Post>> call= akademiseApi.getPosts();
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if(!response.isSuccessful()){
+                    Log.d("Post", "onResponse: not successful");
+                    return;
+                }
+
+                List<Post> posts = response.body();
+                int count=0;
+                for (Post post : posts){
+                    count++;
+                }
+                Log.d("Post", String.valueOf(count));
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+
+                Log.d("Post", "onFailure: failed");
+
+            }
+        });
+
+    }
+
+    private void createPost(){
+    Post post = new Post(22, "title", "text");
+    Call<Post> call = akademiseApi.createPost(post);
+    call.enqueue(new Callback<Post>() {
+        @Override
+        public void onResponse(Call<Post> call, Response<Post> response) {
+
+            if(!response.isSuccessful()){
+                Log.d("Post", "onResponse: not successful");
+                return;
+            }
+
+            Log.d("Post", "onResponse: successful");
+        }
+
+        @Override
+        public void onFailure(Call<Post> call, Throwable t) {
+            Log.d("Post", "onFailure: failed");
+        }
+    });
+    }
+
 }
