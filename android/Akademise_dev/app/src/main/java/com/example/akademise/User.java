32ccf45f2448589285bc88a6fc3c2be0bbc910be
DogukanKalkan <dogukan.kalkan@boun.edu.tr>
2020-11-22 14:57:12 +0300

    User class is added.

---
+++ android/Akademise_dev/app/src/main/java/com/example/akademise/User.java
@@ -0,0 +1,35 @@
+package com.example.akademise;
+
+public class User {
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String surname;
+
+    //SIGNUP
+    public User(String email, String password, String name, String surname) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
