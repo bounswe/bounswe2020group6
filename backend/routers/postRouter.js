b95c2149c7dbfa8e3119ad8e97aaa15adf26b9a2
hazerb <60961048+hazerb@users.noreply.github.com>
2020-11-22 12:39:50 +0300

    Update postRouter.js

--- backend/routers/postRouter.js
+++ backend/routers/postRouter.js
@@ -5,7 +5,7 @@ const postController = require('../controllers/postController')
 //routes for adding,updating,deleting and getting posts. '/add' route also handles file uploads
 router.post('/add',postController.upload.any(),postController.addPost)
 router.patch('/update/:id', postController.updatePost)
-router.post('/delete', postController.deletePost)
+router.delete('/delete/:id', postController.deletePost)
 router.get('/get/:userId', postController.getPosts)
 
 
