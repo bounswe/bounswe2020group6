0ace6e57717511494088d22d7be216cc29e96417
Emilcan Arican <emilcan.arican@boun.edu.tr>
2020-11-23 18:15:52 +0300

    recommendation card is fixed
    corrections are made related to tags of the project recommendation card

--- frontend/src/components/ProjectRecommendationCard/index.jsx
+++ frontend/src/components/ProjectRecommendationCard/index.jsx
@@ -6,7 +6,7 @@ import {
 } from "./style";
 import theme from "../../theme";
 
-const ProjectRecommendationCard = ({ name, commoncolabsnum}) => {
+const ProjectRecommendationCard = ({ name, commoncolabsnum, tags}) => {
   return (
     <Layout wrap={false} xs={18} sm={18} md={18} lg={18}>
       <Col flex="40px">
@@ -19,15 +19,11 @@ const ProjectRecommendationCard = ({ name, commoncolabsnum}) => {
         </Title>
       </Row>
       <Tags>
-      <Tag color={theme.main.colors.first} style={{borderRadius: "8px", height: "16px", fontSize: "10px", lineHeight: "14px"}}>
-          Deep Learning
-        </Tag>
-        <Tag color={theme.main.colors.first} style={{borderRadius: "8px", height: "16px", fontSize: "10px", lineHeight: "14px"}}>
-          Quantum Computing
-        </Tag>
-        <Tag color={theme.main.colors.first} style={{borderRadius: "8px", height: "16px", fontSize: "10px", lineHeight: "14px"}}>
-          Blockchain
-        </Tag>
+        { tags !== undefined ? tags.map((t) => {
+          return <Tag color={theme.main.colors.first} style={{borderRadius: "8px", height: "16px", fontSize: "10px", lineHeight: "14px"}}>
+            {t}
+          </Tag>
+        }): ":S"}
       </Tags>
       </Col>
     </Layout>
