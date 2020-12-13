3f83969d4cbd804695d21d08bdd99be10a1d3aba
hazerb <60961048+hazerb@users.noreply.github.com>
2020-12-13 16:13:11 +0300

    Update uploadUtil.js

--- backend/util/uploadUtil.js
+++ backend/util/uploadUtil.js
@@ -2,21 +2,21 @@ const multer = require('multer')
 const fs = require('fs');
 
 const storage = multer.diskStorage({
-	destination: './uploads/',
-	filename: function(req, file, cb){
-		cb(null, file.originalname);
-  }
+    destination: './uploads/',
+    filename: function(req, file, cb){
+	cb(null, file.originalname);
+    }
 });
 
 var moveFile = function(fileName,oldPath,newPath) { 
-	if (!fs.existsSync(newPath)) {
-		fs.mkdirSync(newPath);
-	}
-	newPath = `${newPath}/${fileName}`
-	fs.rename(oldPath, newPath, function (err) {
-  	if (err) throw err
- 		console.log('Successfully moved')
-	})
+    if (!fs.existsSync(newPath)) {
+	fs.mkdirSync(newPath);
+    }
+    newPath = `${newPath}/${fileName}`
+    fs.rename(oldPath, newPath, function (err) {
+        if (err) throw err
+ 	console.log('Successfully moved')
+    })
 }
 
 
