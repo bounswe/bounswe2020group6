fd7e8072fb136ea84a0824ac0afca59530a96e11
hazerb <60961048+hazerb@users.noreply.github.com>
2020-12-13 14:30:10 +0300

    Add files via upload
    Added collaboration requests model

---
+++ backend/model/collab_requests.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, Seq) => {
+	return sequelize.define('collab_request', {
+		id : {
+			type : Seq.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		requesterId : {
+			type : Seq.INTEGER,
+			allowNull : false,
+		},
+		requestedId :{
+			type : Seq.INTEGER,
+			allowNull : false,
+		},
+		projectId : {
+			type : Seq.INTEGER,
+			allowNull : false,
+		},
+		}, 
+	{
+		timestamps : true
+	})
+}
