0e94f0770603f4a7014f82fcfbe718b932a3110d
hazerb <60961048+hazerb@users.noreply.github.com>
2020-12-13 16:05:41 +0300

    Update collabUtil.js

--- backend/util/collabUtil.js
+++ backend/util/collabUtil.js
@@ -2,23 +2,23 @@ const {CollabRequest, User} = require("../model/db")
 
 
 var requestExists = async function(requesterId,requestedId,projectId){
-	requestDb = await CollabRequest.findOne({
-		where : {
-			requesterId : requesterId,
-			requestedId : requestedId,
-			projectId : projectId
-		},
-		include : [
-			{
-				model : User,
-				attributes : ['name','surname']
-			}
-		]
-	});
-	if(requestDb){
-		return requestDb
-	}
-	return undefined
+    requestDb = await CollabRequest.findOne({
+	where : {
+	    requesterId : requesterId,
+	    requestedId : requestedId,
+	    projectId : projectId
+	},
+	include : [
+	    {
+	        model : User,
+		attributes : ['name','surname']
+	    }
+	]
+    });
+    if(requestDb){
+	return requestDb
+    }
+    return undefined
 }
 
 
