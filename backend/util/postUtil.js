ec4fad4d5e9c5d2a7bfdac0f5f5bcd305795d7cb
hazerb <60961048+hazerb@users.noreply.github.com>
2020-12-13 16:09:25 +0300

    Update postUtil.js

--- backend/util/postUtil.js
+++ backend/util/postUtil.js
@@ -3,33 +3,33 @@ const {ProjectTag, Project} = require("../model/db")
 
 
 var postExists = async function(projectId){
-	console.log("eyvah")
-	postDb = await Project.findOne({
-		where : {
-			id  : projectId
-		}
-	});
-	if(postDb){
-		return true
+    console.log("eyvah")
+    postDb = await Project.findOne({
+        where : {
+	    id  : projectId
 	}
-	return false
+    });
+    if(postDb){
+	return true
+    }
+    return false
 }
 
 
 
 var tagExists = async function(tags,projectId){
-	for(tag of tags){
-		tagDb = await ProjectTag.findOne({
-			where : {
-				project_id  : projectId,
-				tag : tag
-			}
-		});
-		if(tagDb){
-			return tagDb
-		}
+    for(tag of tags){
+	tagDb = await ProjectTag.findOne({
+	    where : {
+		project_id  : projectId,
+		tag : tag
+	    }
+	});
+	if(tagDb){
+	    return tagDb
 	}
-	return undefined
+    }
+    return undefined
 }
 
 
