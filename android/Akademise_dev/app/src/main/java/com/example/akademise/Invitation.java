b1c2491aa02f3b0bd254f80ed8c1949edd768793
gulperii <ezgi.gulperi@gmail.com>
2020-12-26 20:52:06 +0300

    invitation

---
+++ android/Akademise_dev/app/src/main/java/com/example/akademise/Invitation.java
@@ -0,0 +1,26 @@
+package com.example.akademise;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Invitation implements Serializable {
+    @SerializedName("invitations")
+    private List<List<Integer>>invitations ;
+
+
+    public Invitation(List<Integer> details) {
+        List<List<Integer>> i = new ArrayList<>();
+        i.add(details);
+        this.invitations = i;
+    }
+
+    public List<Integer> getinvitation() {
+        return invitations.get(0);
+    }
+
+}
+
--- android/Akademise_dev/app/src/main/java/com/example/akademise/ProjectDetailsActivity.java
+++ android/Akademise_dev/app/src/main/java/com/example/akademise/ProjectDetailsActivity.java
@@ -2,7 +2,10 @@ package com.example.akademise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ public class ProjectDetailsActivity extends AppCompatActivity {
     TextView milestones;
     TextView requirements;
     TextView tags;
+    Button invite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +26,11 @@ public class ProjectDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_project_details);
         title = findViewById(R.id.title);
         _abstract = findViewById(R.id.tvAbstractProject);
-        status=findViewById(R.id.tvStatusProject);
-        milestones=findViewById(R.id.tvMilestoneProject);
-        requirements=findViewById(R.id.tvRequirementsProject);
-        tags=findViewById(R.id.tvTagsProject);
+        status = findViewById(R.id.tvStatusProject);
+        milestones = findViewById(R.id.tvMilestoneProject);
+        requirements = findViewById(R.id.tvRequirementsProject);
+        tags = findViewById(R.id.tvTagsProject);
+        invite = findViewById(R.id.btnInvite);
 
         getData();
         title.setText(project.getTitle());
@@ -34,16 +39,33 @@ public class ProjectDetailsActivity extends AppCompatActivity {
         milestones.setText(project.getDeadline());
         requirements.setText(project.getRequirements());
 
+        invite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("project_id", project.getId());
+                RequestInvitationFragment frg = new RequestInvitationFragment();
+                frg.setArguments(bundle);
+                getSupportFragmentManager().beginTransaction().replace(R.id.req_and_invitations,
+                        frg).commit();
+            }
+        });
+
 
     }
 
-    private void getData(){
-       String test= getIntent().getClass().toString();
-       //Toast.makeText(this, test, Toast.LENGTH_LONG).show();
-        if(getIntent().hasExtra("project")){
+    public void openInvitationActivity() {
+        Intent intent = new Intent(this, RequestInvitationFragment.class);
+        intent.putExtra("project_id", project.getId());
+        startActivity(intent);
+    }
+
+    private void getData() {
+        String test = getIntent().getClass().toString();
+        //Toast.makeText(this, test, Toast.LENGTH_LONG).show();
+        if (getIntent().hasExtra("project")) {
             project = (Project) getIntent().getSerializableExtra("project");
-        }
-        else{
+        } else {
             Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
         }
     }
