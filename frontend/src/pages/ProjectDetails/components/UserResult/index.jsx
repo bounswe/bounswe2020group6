fd027b7d8e26366ef647b001592981567261636f
Emilcan Arican <emilcan.arican@boun.edu.tr>
2020-12-27 23:13:22 +0300

    user result card minor fix

--- frontend/src/pages/ProjectDetails/components/UserResult/index.jsx
+++ frontend/src/pages/ProjectDetails/components/UserResult/index.jsx
@@ -10,9 +10,9 @@ import {
 const userResult = ({ id, img, name, department, university, profileLink, selected, buttonClicked }) => {
 
   return (  
-    <Layout key={id} wrap={false} xs={18} sm={18} md={18} lg={18} onClick={profileLink} style={{cursor:"pointer"}}>
+    <Layout key={id} wrap={false} xs={18} sm={18} md={18} lg={18} >
     <Col flex="40px">
-      <Avatar size={30}  src={img} style={{boxShadow: "0px 4px 4px rgba(0,0,0,0.25)", margin: "10px"}} />
+      <Avatar size={32}  src={img} style={{boxShadow: "0px 4px 4px rgba(0,0,0,0.25)", margin: "10px"}} />
     </Col>
     <Col flex="auto" justify="center">
       <Row wrap={false}>
