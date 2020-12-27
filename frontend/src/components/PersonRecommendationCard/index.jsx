ff9f1bbca71ad133bb4dfd2fecbfcf396e72fc66
Emilcan Arican <emilcan.arican@boun.edu.tr>
2020-12-28 01:57:27 +0300

    person recommenndation card is connected

--- frontend/src/components/PersonRecommendationCard/index.jsx
+++ frontend/src/components/PersonRecommendationCard/index.jsx
@@ -6,26 +6,39 @@ import {
   Tags
 } from "./style";
 import theme from "../../theme";
-
+import { follow } from "../../redux/follow/api";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import defaultProfilePictureHref from "../../assets/asset_hrefs"
 
-const PersonRecommendationCard = ({ name, commoncolabsnum}) => {
+const PersonRecommendationCard = ({ id, name, university, department}) => {
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const handleFollow = () => {
+    dispatch(follow(id))
+  }
+
   return (
-    <Layout wrap={false} xs={18} sm={18} md={18} lg={18}>
-      <Col flex="40px">
+    <Layout key={id} wrap={false} xs={18} sm={18} md={18} lg={18}>
+      <Col flex="40px" onClick={() => history.push("/profile/" + id)} style={{cursor: "pointer"}}>
       <Avatar size={44}  src={defaultProfilePictureHref} style={{boxShadow: "0px 4px 4px rgba(0,0,0,0.25)", margin: "10px"}} />
       </Col>
       <Col flex="auto" justify="start">
-      <Row wrap={false}>
+      <Row wrap={false} onClick={() => history.push("/profile/" + id)} style={{cursor: "pointer"}}>
         <Title>
           {name}
         </Title>
       </Row>
     <Info>
-      {(commoncolabsnum === 1) ? (commoncolabsnum + " common collaborator") : (commoncolabsnum > 1 ? commoncolabsnum + " common collaborators" : ".")}
+      {university} {department}
     </Info>
       <Tags>
-        <Tag color={theme.main.colors.first} style={{borderRadius: "8px", height: "16px", fontSize: "10px", lineHeight: "14px"}}>
+        <Tag 
+        onClick={() => handleFollow()}
+        color={theme.main.colors.first} 
+        style={{borderRadius: "8px", height: "16px", fontSize: "10px", lineHeight: "14px", cursor: "pointer"}}>
           +Follow
         </Tag>
       </Tags>
