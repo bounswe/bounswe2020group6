8d53b22f38880e8f98838f4ce8b756e5a1a4befc
hazerb <60961048+hazerb@users.noreply.github.com>
2021-01-21 15:33:21 +0300

    fixed indentation

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -1,48 +1,48 @@
 const {User, Project, Notification}  = require('../model/db')
 
 deleteNotification = async function(req,res){
-	try{
-		id = req.params.id
-		await Notification.destroy({
-			where : {
-				id : id
-			}
-		});
-		res.status(201).send({message: "Notification is deleted"})
-	}catch(error){
-		res.status(500).send(error.message)
-	}
+    try{
+	id = req.params.id
+	await Notification.destroy({
+	    where : {
+	        id : id
+	    }
+        });
+	res.status(201).send({message: "Notification is deleted"})
+    }catch(error){
+	res.status(500).send(error.message)
+    }
 }
 
 
 getNotifications = async function(req,res){
-	try{
+    try{
         userId = req.userId
-	    notifications = await Notification.findAll({
-		    where : {
-			    receiverId : userId
-		    },
-		    include : [
-			    {
-				    model : User,
-				    as : 'accepter',
-				    attributes : ['name','surname'],
-				    required : false
-			    },
-			    {
-				    model : User,
-				    as : 'participant',
-				    attributes : ['id','name','surname'],
-				    required : false
-			    },
-			    {
-				    model : Project,
-				    attributes : ['id','title','summary','description','status'],
-				    required : false
-	    	    }
-		    ]
-	    });
-	    res.status(200).send(notifications)
+	notifications = await Notification.findAll({
+            where : {
+		receiverId : userId
+	    },
+            include : [
+		{
+                    model : User,
+		    as : 'accepter',
+		    attributes : ['name','surname'],
+		    required : false
+		},
+		{
+		    model : User,
+		    as : 'participant',
+		    attributes : ['id','name','surname'],
+		    required : false
+	        },
+	        {
+		    model : Project,
+		    attributes : ['id','title','summary','description','status'],
+		    required : false
+	    	}
+	    ]
+        });
+	res.status(200).send(notifications)
     }catch(error){
         res.status(500).send(error.message)
     }
