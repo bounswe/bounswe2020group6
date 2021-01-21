f48d7086db08816a2963450f56aadec1315773e5
hazerb <60961048+hazerb@users.noreply.github.com>
2021-01-21 15:44:49 +0300

    fixed indentation

--- backend/util/notificationUtil.js
+++ backend/util/notificationUtil.js
@@ -1,18 +1,18 @@
 const {Notification} = require("../model/db")
 
 var addNotification = async function(projectId,type,accepterId,participantId,receiverId,body){
-	notification = {
-	    projectId : projectId,
-		  type : type,
-		  accepterId : accepterId,
-		  participantId : participantId,
-		  receiverId : receiverId,
-		  body : body
-  }
-	notificationDb = await Notification.create(notification)
+    notification = {
+        projectId : projectId,
+        type : type,
+        accepterId : accepterId,
+        participantId : participantId,
+        receiverId : receiverId,
+        body : body
+    }
+    notificationDb = await Notification.create(notification)
 }
 
 
 module.exports = {
-   addNotification
+    addNotification
 }
