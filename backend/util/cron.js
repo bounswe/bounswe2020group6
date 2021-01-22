12c18a03adf922a4611ba87026cd37b88cc78d4c
@muhammedenes.toptas <muhammedenes.toptas@tokeninc.com>
2021-01-22 22:35:10 +0300

    cron job to delete non-validated users

---
+++ backend/util/cron.js
@@ -0,0 +1,22 @@
+const cron = require('node-cron');
+const { User } = require('../model/db')
+ 
+//will delete non-validated users older than one month, once every month
+const task = cron.schedule('0 0 1 * *', () => {
+  let date = new Date()
+  date.setDate( date.getDate() - 30 )
+  const num = await User.destroy({
+      where: {
+          isValidated: false,
+          createdAt: {
+              [Op.lte]: date
+          }
+      }
+  })
+  console.log(`Removed ${num} users at` )
+
+}, { scheduled: false });
+
+
+
+module.exports = task
