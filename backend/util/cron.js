bc03305421e3e6f5df45f70b89b8da0d70596e16
Muhammed Enes Toptaş <44070728+EnesToptas@users.noreply.github.com>
2021-01-23 15:35:17 +0300

    make function async

--- backend/util/cron.js
+++ backend/util/cron.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const { User } = require('../model/db')
  
 //will delete non-validated users older than one month, once every month
-const task = cron.schedule('0 0 1 * *', () => {
+const task = cron.schedule('0 0 1 * *', async () => {
   let date = new Date()
   date.setDate( date.getDate() - 30 )
   const num = await User.destroy({
