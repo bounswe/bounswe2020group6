652511b63eceedbeb9a4b7a1e7a23cb23324ab4a
Emilcan Arican <emilcan.arican@boun.edu.tr>
2021-01-24 18:13:11 +0300

    event edit api is added
    event edit api is added

--- frontend/src/redux/event/api.js
+++ frontend/src/redux/event/api.js
@@ -1,8 +1,10 @@
 import api from "../../axios";
+import moment from 'moment';
 
 // sends a post request to create an event
 export const postEvent = (body, history, message) => {
   return (dispatch) => {
+    // send post request
     api({ sendToken: true })
       .post("/event/add", body)
       .then((response) => {
@@ -19,4 +21,46 @@ export const postEvent = (body, history, message) => {
   };
 };
 
+// gets the event information
+export const getEvent = (eventId, history, setEventData, setloading) => {
+  return (dispatch) => {
+    // send get request
+    api({ sendToken: true })
+      .get("/event/event/" + eventId)
+      .then((response) => {
+        /*
+        if (! response.data.length > 0){
+          history.goBack();
+        }
+        */
+        setEventData(() => {
+          var next = {
+            ...response.data.result,
+            tags: response.data.result.event_tags.map(t => t.tag), 
+          }
+          next.date = moment(new Date(next.date))
+          return next
+        });
+        setloading(false)
+      })
+      .catch((e) => {
+        console.log(e)
+      });
+  };
+};
 
+// edit the post
+export const editEvent = (body, eventId, history, message) => {
+  return (dispatch) => {
+    // send patch request
+    api({ sendToken: true })
+      .patch("/event/update/" + eventId, body)
+      .then((response) => {
+        message.success("Event changes saved.", 4);
+        history.push("/event/details/" + eventId);
+      })
+      .catch((e) => {
+        message.error("Event changes failed.", 4);
+      });
+  };
+};
