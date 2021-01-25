65668eb94637c5e517115e2a3d030e5083993154
Emilcan Arican <emilcan.arican@boun.edu.tr>
2021-01-25 23:32:52 +0300

    saved events button is added
    saved events button is added to the profile sider component

--- frontend/src/components/ProfileSider/index.jsx
+++ frontend/src/components/ProfileSider/index.jsx
@@ -10,6 +10,7 @@ import {
   ContainerOutlined,
   CalendarOutlined,
   UnorderedListOutlined,
+  AppstoreAddOutlined,
 } from "@ant-design/icons";
 import { Layout, NameText, Img } from "./style";
 import GoogleScholarModal from "../GoogleScholarModal";
@@ -111,6 +112,13 @@ const ProfileSider = () => {
                 icon={<UnorderedListOutlined />}
               >
                 Events
+              </Menu.Item>
+              <Menu.Item
+                key="6"
+                onClick={() => history.push("/event/saved")}
+                icon={<AppstoreAddOutlined />}
+              >
+                Saved Events
               </Menu.Item>
             </Menu>
           </div>
